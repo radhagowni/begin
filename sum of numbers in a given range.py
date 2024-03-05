61a78f239b2b58845d4f9c229eef2dd12868badf
radhagowni <136911875+radhagowni@users.noreply.github.com>
2024-03-05 21:33:17 +0530

    Add files via upload

---
+++ sum of numbers in a given range.py
@@ -0,0 +1,14 @@
+print('first method:')
+a=int(input('enter a value:'))
+b=int(input('enter value:'))
+sum=0
+for i in range(a,b+1):
+    sum=sum+i
+print(sum)
+print('time complexity is O(n)')
+print('space complexity is O(1)')
+print('alternate method:')
+sum=int((b*(b+1)/2)-(a*(a+1)/2))+a 
+print("sum is",sum)
+print('time complexity : O(1)')
+print('space complexity :O(1)')
