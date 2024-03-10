cec4501f85d7d982a49f5b6e9b0270a0ccc8e270
radhagowni <136911875+radhagowni@users.noreply.github.com>
2024-03-10 08:21:26 +0530

    Add files via upload

---
+++ prime number in a given range.py
@@ -0,0 +1,16 @@
+import math
+def checkprime(num):
+    if num<2:
+        return 0
+    else:
+        x=int(num/2)
+        for i in range( 2,x+1):
+            if num%i==0:
+                return 0
+        return 1
+print('enter lower and upper ranges:')
+a=int(input())
+b=int(input())
+for i in range(a,b):
+    if checkprime(i):
+        print(i,end=' ')
