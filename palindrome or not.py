74e61a1b4b94cd1d0a81e164febf80f9dbd889c4
radhagowni <136911875+radhagowni@users.noreply.github.com>
2024-03-15 16:07:42 +0530

    Add files via upload

---
+++ palindrome or not.py
@@ -0,0 +1,17 @@
+n=int(input())
+temp=n
+reverse=0
+while temp>0:
+    rem=temp%10
+    reverse=(reverse*10)+rem
+    temp=temp//10
+if n==reverse:
+    print(n, 'is palindrome')
+else:
+    print('not a palindrome')
+print('alternate method:')
+reverse=int(str(n)[::-1])
+if n==reverse:
+    print(n,' is palindrome')
+else:
+    print('not a palindrome')
