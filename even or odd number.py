da60c0ff14c89cb61526dbde3bcc199be370ee36
Gowni Radha <136911875+radhagowni@users.noreply.github.com>
2025-08-08 07:33:20 +0530

    Update even or odd number.py

--- even or odd number.py
+++ even or odd number.py
@@ -1,8 +1,8 @@
 num=int(input('enter num:'))
 if (num%2==0):
-    print('even number')
+    print(num,' even number')
 else:
-    print('odd number')
+    print(num,' odd number')
 #alternate method
 print('alternate method output :')
 print('even number' if num%2==0  else "odd number")
