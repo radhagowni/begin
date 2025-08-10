520faf6f2d29c09a4315e7839cdd86064705faf8
Gowni Radha <136911875+radhagowni@users.noreply.github.com>
2025-08-10 17:45:46 +0530

    Update postive or negative number.py

--- postive or negative number.py
+++ postive or negative number.py
@@ -1,11 +1,11 @@
 num=int(input('enter a number:'))
 if num>=0:
     if num>0:
-        print("positive number")
+        print(num," positive number")
     else:
         print('0')
 else:
-    print('negative number')
+    print(num,' negative number')
 #alternate method
 print('alternate method output:')
 print('positive' if num>=0 else 'negative')
